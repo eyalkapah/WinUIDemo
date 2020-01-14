3b87ddfc1c08d11de2f0f904471cc518bfb4293b
Eyal.Kapah <eykapah@microsoft.com>
2020-01-14 16:41:47 +0200

    Add project files.

---
+++ UI/MainShell.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace UI
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainShell : Page
+    {
+        public bool IsOpen
+        {
+            get { return (bool)GetValue(IsOpenProperty); }
+            set { SetValue(IsOpenProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for IsOpen.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty IsOpenProperty =
+            DependencyProperty.Register("IsOpen", typeof(bool), typeof(MainShell), new PropertyMetadata(default));
+
+        public MainShell()
+        {
+            this.InitializeComponent();
+
+            this.Loaded += MainShell_Loaded;
+        }
+
+        private void MainShell_Loaded(object sender, RoutedEventArgs e)
+        {
+            //IsOpen = true;
+        }
+
+        private void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                IsOpen = true;
+            }
+            catch (Exception ex)
+            {
+            }
+        }
+    }
+}
